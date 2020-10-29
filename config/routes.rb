92871b02036fdb12c553ebaf8025f177826482db
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-10-29 20:21:42 +0900

    画面遷移の実

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: "rooms#index"
   get 'messages/index'
-  root to: "messages#index"
+  #root to: "messages#index"       質問終わったら消す
   resources :users, only: [:edit, :update]
   resources :rooms, only: [:new, :create]
 end
