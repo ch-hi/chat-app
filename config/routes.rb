298ca157fa07be7b84772aa395d13d3b27dc8cf2
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-10-30 18:11:05 +0900

    チャットルームの削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   #root to: "messages#index"       質問終わったら消す
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create] do
+  resources :rooms, only: [:new, :create, :destroy] do
     resources :messages, only: [:index, :create]
   end
 end
