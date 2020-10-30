8b75c4857cfd32d464e9761c4c65fdc047d244c0
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-10-30 16:06:14 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'messages/index'
   #root to: "messages#index"       質問終わったら消す
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
 end
