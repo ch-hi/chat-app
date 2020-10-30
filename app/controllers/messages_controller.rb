71f23dcd8fc2a66e9d20707a32aab5fa1ca1fdf9
ch-hi <73207820+ch-hi@users.noreply.github.com>
2020-10-30 19:23:19 +0900

    Active Storageの導入

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -25,6 +25,6 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:content、 :image).merge(user_id: current_user.id)
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
